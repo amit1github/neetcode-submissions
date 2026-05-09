a1119ca716667cc95625b73425042e2f9fac9827
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2026-05-09 10:27:47 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        let s_sort = s.split('').sort().join('');
+        let t_sort = t.split('').sort().join('');
+
+        if (s_sort === t_sort) {
+            return true
+        }
+
+        return false;
+    }
+}
