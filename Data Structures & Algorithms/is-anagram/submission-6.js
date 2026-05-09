706d2005fa079b17129279686c5fc60850f5af37
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2026-05-09 11:30:19 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        let s_sort = s.split("").sort().join("");
+        let t_sort = t.split("").sort().join("");
+
+        return s_sort === t_sort;
+    }
+}
